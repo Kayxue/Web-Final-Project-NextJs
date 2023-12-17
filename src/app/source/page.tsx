4481a2686a9704490a12cf5a0ac23d24ad3807ed
Kayxue <s0990006@gmail.com>
2023-12-17 16:30:37 +0800

    2023/12/17 Finished Source Page

--- src/app/source/page.tsx
+++ src/app/source/page.tsx
@@ -8,7 +8,7 @@ import "../../../resources/Container.css"
 export default function aboutus() {
     return (
         <div>
-            <h1><span>頁面尚未完成</span></h1>
+            <h1><span>此網頁所用素材連結</span></h1>
 
             <div className="go_home">
                 <Link href="/" passHref legacyBehavior>
@@ -16,6 +16,48 @@ export default function aboutus() {
                         <Image src={homeBtn} alt="" />
                     </a>
                 </Link>
+            </div>
+
+            <div className="board">
+                <table>
+                    <tbody>
+                        <tr key="1">
+                            <td className="inserted">
+                                <a href="https://www.flaticon.com/free-icons/queue" title="queue icons">Queue icons created by Freepik - Flaticon</a>
+                            </td>
+                        </tr>
+                        <tr key="2">
+                            <td className="inserted">
+                                <a href="https://www.flaticon.com/free-icons/bundle" title="bundle icons">Bundle icons created by Amazona Adorada - Flaticon</a>
+                            </td>
+                        </tr>
+                        <tr key="3">
+                            <td className="inserted">
+                                <a href="https://www.flaticon.com/free-icons/info" title="info icons">Info icons created by Freepik - Flaticon</a>
+                            </td>
+                        </tr>
+                        <tr key="4">
+                            <td className="inserted">
+                                <a href="https://www.flaticon.com/free-icons/array" title="array icons">Array icons created by Lizel Arina - Flaticon</a>
+                            </td>
+                        </tr>
+                        <tr key="5">
+                            <td className="inserted">
+                                <a href="https://www.flaticon.com/free-icons/sourcing" title="sourcing icons">Sourcing icons created by Andrean Prabowo - Flaticon</a>
+                            </td>
+                        </tr>
+                        <tr key="6">
+                            <td className="inserted">
+                                <a href="https://www.flaticon.com/free-icons/home" title="home icons">Home icons created by Freepik - Flaticon</a>
+                            </td>
+                        </tr>
+                        <tr key="7">
+                            <td className="inserted">
+                                <a href="https://www.flaticon.com/free-icons/ui" title="ui icons">Ui icons created by NajmunNahar - Flaticon</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
